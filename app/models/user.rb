d3a5a06a1c35f028da96ccaf017b1f74dede0414
Nicolas Cureau <nicolascureau@gmail.com>
2023-11-28 12:40:38 +0100

    adding seeds (user, event, survey) 28.11 / 12.40

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,4 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 4 }, confirmation: true
   validates :password_confirmation, presence: true
-
 end
