c3d3bcad9b009687a4f08c072ecda8f436f4d222
Jennifer Crétal <jennifer.cretal@gmail.com>
2023-11-29 13:39:39 +0100

    Create survey ok

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,8 +1,10 @@
 class Survey < ApplicationRecord
+  CATEGORIES = %w(Date Lieu Autre)
   belongs_to :user
   belongs_to :event
-  has_many :answers
+  has_many :answers, dependent: :destroy
   # belongs_to :user_hidden, optional: true
   validates :question, presence: true
   validates :deadline, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
