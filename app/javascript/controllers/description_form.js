25f9f433850f83442a32212658ea2c6efb45a606
Nicolas Cureau <nicolascureau@gmail.com>
2023-12-01 15:32:38 +0100

    resole bug

--- app/javascript/controllers/description_form.js
+++ app/javascript/controllers/description_form.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const editButton = document.querySelector('.edit-description');
-  const editForm = document.querySelector('.edit-description-form');
-  const description = document.querySelector('.description')
+// document.addEventListener('DOMContentLoaded', function() {
+//   const editButton = document.querySelector('.edit-description');
+//   const editForm = document.querySelector('.edit-description-form');
+//   const description = document.querySelector('.description')
 
-  editButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log(editForm);
-    console.log(description);
+//   editButton.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     console.log(editForm);
+//     console.log(description);
 
-    editForm.classList.toggle('d-none');
-    description.classList.toggle('d-none');
+//     editForm.classList.toggle('d-none');
+//     description.classList.toggle('d-none');
 
-  });
-});
+//   });
+// });
