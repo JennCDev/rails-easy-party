8cec299355310c893531798f20ab0b78a4b0bd21
Nicolas Cureau <nicolascureau@gmail.com>
2023-12-05 11:32:47 +0100

    newmessages

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -5,7 +5,7 @@ class UserEventsController < ApplicationController
     @event = Event.find(params[:event_id])
     @user_event = UserEvent.new
     @users = User.all
-    @participants = @event.user_events.includes(:user).where(status: 'going')
+    @guests = @event.user_events.includes(:user).where(status: 'pending')
   end
 
   def create
@@ -13,29 +13,33 @@ class UserEventsController < ApplicationController
     existing_user_event = @event.user_events.find_by(user_id: user_id)
 
     if existing_user_event
+      # Redirection si l'utilisateur est déjà inscrit à l'événement
       redirect_to event_path(@event), notice: "L'utilisateur est déjà ajouté à l'événement."
     else
       @user_event = @event.user_events.new(user_event_params)
       @user_event.user_id = user_id
       @user_event.planner = user_event_params[:planner] == '1'
-      if @user_event.planner
-        @user_event.status = 'going'
-      else
-        @user_event.status = 'pending'
-      end
+      @user_event.status = @user_event.planner ? 'going' : 'pending'
 
       if @user_event.save
+        # Redirection en cas de succès
         redirect_to event_path(@event), notice: "Utilisateur ajouté à l'événement avec succès!"
       else
+        # Préparation de la vue 'new' en cas d'erreur
         flash.now[:alert] = "Impossible d'ajouter l'utilisateur à l'événement."
+        @guest = @event.user_events.includes(:user).where(status: 'pending')
         render :new, status: 422
       end
     end
+  end
+
 
     def show
       @event = Event.find(params[:id])
     end
-  end
+
+
+
 
   private
 
