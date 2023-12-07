16710ebf5b68d7f8dc26af76d7d05a5b17d84f48
Jennifer Crétal <jennifer.cretal@gmail.com>
2023-12-07 17:10:57 +0100

    fix btn form

--- app/javascript/controllers/insert_in_answers_controller.js
+++ app/javascript/controllers/insert_in_answers_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
       const html = `<div data-insert-in-answers-target="answer" style="margin-bottom: 20px;">
       <div class="position-relative">
         <input class="form-control input string optional pr-4" type="text" name="survey[answer][]" value="${this.inputTarget.value}" readonly>
-        <i class="fa-solid fa-xmark fa-xmark-survey position-absolute top-0 end-0 mt-1 me-2" style="cursor: pointer;" data-action="click->insert-in-answers#deleteInput"></i>
+        <i class="fa-solid fa-xmark position-absolute top-0 end-0 mt-1 me-2" style="cursor: pointer;" data-action="click->insert-in-answers#deleteInput"></i>
       </div>
     </div>`;
 
--- app/javascript/controllers/option_controller.js
+++ app/javascript/controllers/option_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
       const html = `<div style="margin-bottom: 20px;">
       <div class="position-relative">
         <input class="form-control input string optional pr-4" type="text" name="option[]" value="${this.optionsTarget.value}" readonly>
-        <i class="fa-solid fa-xmark fa-xmark position-absolute top-0 end-0 mt-1 me-2" style="cursor: pointer;" data-action="click->option#deleteInput"></i>
+        <i class="fa-solid fa-xmark position-absolute top-0 end-0 mt-1 me-2" style="cursor: pointer;" data-action="click->option#deleteInput"></i>
       </div>
       </div>`;
       this.optionsListTarget.insertAdjacentHTML('beforeend', html);
