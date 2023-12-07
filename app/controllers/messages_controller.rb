3dd46a18bd77a41473c4bdc7214734f1747d1b57
Jennifer Crétal <jennifer.cretal@gmail.com>
2023-12-07 19:49:49 +0100

    test fix chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 # app/controllers/messages_controller.rb
 class MessagesController < ApplicationController
+  include CloudinaryHelper
+
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
@@ -10,7 +12,7 @@ class MessagesController < ApplicationController
         @chatroom,
         message: render_to_string(partial: "message", locals: { message: @message }),
         sender_id: @message.user.id,
-        avatar_url: @message.user.avatar.attached? ? url_for(@message.user.avatar) : nil
+        avatar_url: @message.user.avatar.attached? ? cl_image_path(@message.user.avatar.key) : nil
       )
 
     else
