d822a56d9c32893062bf90fe3f5613566192110a
Nicolas Cureau <nicolascureau@gmail.com>
2023-12-07 18:43:15 +0100

    last fix

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -12,7 +12,7 @@ class UserEventsController < ApplicationController
     existing_user_event = @event.user_events.find_by(user_id: user_id)
 
     if existing_user_event
-      redirect_to event_path(@event), notice: "L'utilisateur est déjà ajouté à l'événement."
+      redirect_to new_event_user_event_path(@event), notice: "L'utilisateur est déjà ajouté à l'événement."
     else
       @user_event = @event.user_events.new(user_event_params)
       @user_event.user_id = user_id
@@ -20,7 +20,7 @@ class UserEventsController < ApplicationController
       @user_event.status = @user_event.planner ? 'going' : 'pending'
 
       if @user_event.save
-        redirect_to event_path(@event), notice: "Utilisateur ajouté à l'événement avec succès!"
+        redirect_to new_event_user_event_path(@event), notice: "Utilisateur ajouté à l'événement avec succès!"
       else
         flash.now[:alert] = "Impossible d'ajouter l'utilisateur à l'événement."
         render :new, status: 422
